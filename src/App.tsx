c6541b86269b970245f4565738ffc5c1e125efea
unknown <marcelinogs2001@gmail.com>
2023-07-03 12:01:40 -0300

    configure base project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <p className="font-oswald text-5xl text-primary absolute bottom-1/2 left-1/2 -translate-x-1/2 ">
+        Bom trabalho, galera! :)
+      </p>
+    </div>
+  );
+}
+
+export default App;
