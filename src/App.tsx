7fab23d117612753cdc5f4f6716c1a2303878a00
Virgynia Lopes Baccan <virgyniabaccan@gmail.com>
2023-07-04 01:43:17 -0300

    Atualiza estilização Home/ProductPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+// import { Home } from "./components/pages/Home";
+import { ProductPage } from "./components/pages/ProductPage";
+
 function App() {
   return (
     <div>
-      <p className="font-oswald text-5xl text-primary absolute bottom-1/2 left-1/2 -translate-x-1/2 ">
-        Bom trabalho, galera! :)
-      </p>
+      <ProductPage />
     </div>
   );
 }
