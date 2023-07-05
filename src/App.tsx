18336c2ad132d07a9877db152ea56c2f3289320e
Virgynia Lopes Baccan <virgyniabaccan@gmail.com>
2023-07-05 10:37:03 -0300

    Atualiza Product

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,16 @@
-// import { AddProductModal } from "./components/AddProductModal";
-// import { EditProductModal } from "./components/EditCardModal";
-// import { Cart } from "./components/Cart";
-import { Home } from "./components/pages/Home";
-// import { ProductPage } from "./components/pages/ProductPage";
+import { AddProductModal } from "./components/AddProductModal";
+import { EditProductModal } from "./components/EditCardModal";
+import { Cart } from "./components/Cart";
+// import { Home } from "./components/pages/Home";
 
 function App() {
   return (
     <>
-    <Home />
-      {/* <ProductPage /> */}
-      {/* <Cart>
+      <Cart>
         <AddProductModal />
         <EditProductModal />
-      </Cart> */}
+      </Cart>
     </>
   );
 }
-
-// import { Header } from "./components/Header";
-//import { Dashboard } from "./components/pages/Dashboard";
-// import { ProductPage } from "./components/pages/ProductPage";
-
-// function App() {
-//   return (
-//     <div>
-// <<<<<<< HEAD
-//       <ProductPage />
-// =======
-//       <Header/>
-//       <ProductPage/>
-// >>>>>>> origin/dev-alvaro
-//     </div>
-// >>>>>>> origin/dev-virgynia
-// =======
-// import { Register } from "./components/pages/SignUp";
-// // import { LoginUser } from "./components/pages/Login";
-
-// function App() {
-//   return (
-//     <>
-//     <Register/>
-//     </>
-// >>>>>>> origin/dev-luis
-//   );
-// }
-
 export default App;
